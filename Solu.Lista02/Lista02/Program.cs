6df3084d42a2bcaa6dba3cb5b979b8053e625235
Allan Namiki <tioitz@hotmail.com>
2024-05-23 22:42:46 -0300

    Terminado

--- Solu.Lista02/Lista02/Program.cs
+++ Solu.Lista02/Lista02/Program.cs
@@ -6,7 +6,9 @@
         {
             //Exercício01();
             //Exercício02();
-            Exercício03();
+            //Exercício03();
+            //Exercício04();
+            Exercício05();
         }
 
         private static void Exercício01()
@@ -107,11 +109,83 @@
         {
             /* -Escreva um programa que declare dois vetores de mesmo tamanho e calcule a soma dos elementos correspondentes de ambos os vetores, 
                 armazenando o resultado em um terceiro vetor. Em seguida, exiba o vetor resultante. */
-        }
 
+            int[] vetorUm = new int[5]; // 0, 0, 0, 0, 0
+            int[] vetorDois = new int[5];
+
+            for (int i = 0; i < vetorUm.Length; i++)
+            {
+                Console.WriteLine("Digite um número inteiro: ");
+                if (int.TryParse(Console.ReadLine(), out int valorInteiro))
+                {
+                    vetorUm[i] = valorInteiro;
+                }
+                else
+                {
+                    Console.WriteLine("Valor invalido. Tente novamente");
+                    i--;
+                }
+            }
+            for (int i = 0; i < vetorDois.Length; i++)
+            {
+                Console.WriteLine("Digite um número inteiro: ");
+                if (int.TryParse(Console.ReadLine(), out int valorInteiro))
+                {
+                    vetorDois[i] = valorInteiro;
+                }
+                else
+                {
+                    Console.WriteLine("Valor invalido. Tente novamente");
+                    i--;
+                }
+            }
+            Console.WriteLine("\n"); // Pra separar o 2 do 4
+            int[] vetorResultante = new int[5]; //var vetorResultante = vetorUm[5] + vetorDois[5]
+
+            for (int i = 0; i < vetorUm.Length; i++)
+            {
+                vetorResultante[i] = vetorUm[i] + vetorDois[i];
+            }
+            imprimirInformacoes(vetorResultante);
+          
+
+
+        }
         private static void Exercício05()
         {
             // - Crie uma matriz 4x4 de números inteiros aleatórios e encontre o maior valor presente nela.
+
+            Random randomizer = new Random();
+            
+            // Generate a random number between 0 and 100
+            // int randomNumber = randomizer.Next(101);
+
+            // Output the random number
+            // Console.WriteLine("Random number: " + randomNumber);
+
+            int[,] vetor = new int[4, 4];
+            
+            for (int i = 0; i < 4; i++) 
+            {   
+               for (int j = 0; j < 4; j++)
+               {
+                    vetor[i, j] = randomizer.Next(0, 100);
+               }                
+            }
+            int maiorNumero = vetor[0, 0];
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (maiorNumero < vetor[i, j])
+                    {
+                        maiorNumero = vetor[i, j];
+                    }
+                }
+            }
+        Console.WriteLine(maiorNumero);
         }
 
         private static void Exercício06()
@@ -131,6 +205,10 @@
             { "Fluffy", "Gato", "2", "4kg" },
             { "Spot", "Cachorro", "4", "12kg" }
             }; */
+
+            string[,] informacoes = new string[5, 4];
+
+            
         }
     }
 }
